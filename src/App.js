99c9df94ef0e9c74c1a1c248e07ec2431a83cf95
arcueku <arcuw74@gmail.com>
2020-04-15 08:33:23 +0900

    AppComponentでリストデータを管理する

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import TodoList from './TodoListComponent'
+import { ListItem } from 'material-ui'
 
 class App extends React.Component {
   render() {
+    const list = ['プログラミング','読書']
+
     return (
       <div>
         <h1>TODOアプリ</h1>
-        <TodoList />
+        <TodoList list = {list} />
       </div>
     )
   }
--- src/TodoListComponent.js
+++ src/TodoListComponent.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 
-function TodoList() {
+function TodoList(props) {
   return (
-    <>
-      <div>プログラミング</div>
-      <div>本を読む</div>
-    </>
+    <List>
+      {props.list.map((item, i) => (
+        <ListItem key={i}>
+          <ListItemText primary={item} />
+        </ListItem>
+      ))}
+    </List>
   )
 }
 export default TodoList
