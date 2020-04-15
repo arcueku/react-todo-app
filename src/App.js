775d4cd5dcc0bfd1015e0e414af110cd019e855d
arcueku <arcuw74@gmail.com>
2020-04-15 09:14:34 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import TodoList from './TodoListComponent'
+import Form from './FormComponent'
 import { ListItem } from 'material-ui'
 
 class App extends React.Component {
-  render() {
-    const list = ['プログラミング','読書']
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [''],
+      value: '',
+    }
+  }
+  handleChange = (event) => {
+    this.setState({ value: event.target.value })
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
 
+    this.setState({
+      list: [...this.state.list, this.state.value],
+      value: '',
+    })
+  }
+  render() {
     return (
       <div>
         <h1>TODOアプリ</h1>
-        <TodoList list = {list} />
+        <Form
+          value={this.state.value}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <TodoList list={this.state.list} />
       </div>
     )
   }
